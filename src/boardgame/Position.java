2ccf1893cbc951e4203feafd83ac16e00930c844
Caio Sergio <caioseal@gmail.com>
2021-11-18 17:54:36 -0300

    Implemeting possible moves of Rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -30,6 +30,11 @@ public class Position {
     }
 
     //Functions
+    public void setValues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
     @Override
     public String toString() {
         return row + ", " + column;
