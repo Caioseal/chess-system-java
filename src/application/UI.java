b1b947b80e97c2ceef02c4b55327364a6566a82f
Caio Sergio <63813910+Caioseal@users.noreply.github.com>
2021-11-24 17:15:29 -0300

    Pawn

--- src/application/UI.java
+++ src/application/UI.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class UI {
 
@@ -41,6 +42,7 @@ public class UI {
         printBoard(chessMatch.getPieces()); //Print the board and the pieces
         System.out.println();
         printCapturedPieces(captured); //Print captured pieces
+        System.out.println();
         System.out.println("Turn " + chessMatch.getTurn());
         if (!chessMatch.getCheckMate()) { //If the game isn't in checkmate
             System.out.println("Waiting player " + chessMatch.getCurrentPlayer());
@@ -92,14 +94,16 @@ public class UI {
     }
 
     private static void printCapturedPieces(List<ChessPiece> captured) {
-        System.out.println("Captured pieces: ");
+        List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+        List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+        System.out.println("Captured pieces:");
         System.out.print("White: ");
         System.out.print(ANSI_WHITE);
-        //Create a list of chess pieces filtering by color
-        System.out.println(Arrays.toString(captured.stream().filter(x -> x.getColor() == Color.WHITE).toArray())); //Print the list of white captured pieces on the board
-        System.out.print(ANSI_YELLOW);
+        System.out.println(Arrays.toString(white.toArray()));
+        System.out.print(ANSI_RESET);
         System.out.print("Black: ");
-        System.out.println(Arrays.toString(captured.stream().filter(x -> x.getColor() == Color.WHITE).toArray()));
+        System.out.print(ANSI_YELLOW);
+        System.out.println(Arrays.toString(black.toArray()));
         System.out.print(ANSI_RESET);
     }
 }
---
+++ src/chess/piece/Pawn.java
@@ -0,0 +1,75 @@
+package chess.piece;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece {
+
+    //Constructor
+    public Pawn(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getRows()];
+
+        Position p = new Position(0, 0);
+
+        if (getColor() == Color.WHITE) {
+            p.setValues(position.getRow() - 1, position.getColumn()); //Pawn can move to the row above
+            if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+                ; //If position exists and there is no piece
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+            p.setValues(position.getRow() - 2, position.getColumn()); //First movement the white pawn can move two rows above
+            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            p.setValues(position.getRow() - 1, position.getColumn() -1); //Capture opponent's piece in the diagonal left
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+
+            }
+
+            p.setValues(position.getRow() - 1, position.getColumn() + 1); //Capture opponent's piece in the diagonal right
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+        } else {
+            p.setValues(position.getRow() + 1, position.getColumn()); //Pawn can move to the row above
+            if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) { //If position exists and there is no piece
+                    mat[p.getRow()][p.getColumn()] = true;
+                }
+
+                p.setValues(position.getRow() + 2, position.getColumn()); //First movement the white pawn can move two rows above
+                Position p2 = new Position(position.getRow() + 1, position.getColumn());
+                if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
+                    mat[p.getRow()][p.getColumn()] = true;
+                }
+
+                p.setValues(position.getRow() + 1, position.getColumn() -1); //Capture opponent's piece in the diagonal left
+                if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+                    mat[p.getRow()][p.getColumn()] = true;
+
+                }
+
+                p.setValues(position.getRow() + 1, position.getColumn() + 1); //Capture opponent's piece in the diagonal right
+                if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+                    mat[p.getRow()][p.getColumn()] = true;
+                }
+
+            }
+        return mat;
+    }
+
+    @Override
+    public String toString() {
+        return "P";
+    }
+}
